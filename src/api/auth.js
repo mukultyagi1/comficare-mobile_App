da7c99db4d234c6076a6254826bc7a2dc963acc7
mukultyagi1 <tyagimukul14@gmail.com>
2026-08-28 22:44:44 +0530

    signin changes

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import { apiGet, apiPost } from '../apiClient';
 
 export const authApi = {
-  signIn: (email, password) => apiPost('/auth/signin', { email, password }),
+  signIn: (email, password) => apiPost('/auth/session', { email, password }),
   me: () => apiGet('/auth/me'),
 };
